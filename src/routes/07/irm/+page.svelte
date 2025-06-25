92c7c72395a4c58bf2d3748bc5a956ba3185d8ac
mariafumes <mariadasilva0307@gmail.com>
2025-06-25 12:33:28 -0400

    origin

---
+++ src/routes/07/irm/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { form } = $props();
+</script>
+
+<form method="post">
+    Nome: <input name="nome" type="text" placeholder="Nome do aprendiz" /><br />
+    Nascimento: <input name="nascimento" type="date" /><br />
+    Força: <input name="forca" type="number" min="1" max="10" /><br />
+    Inteligência: <input name="inteligencia" type="number" min="1" max="10" /><br />
+    Destreza: <input name="destreza" type="number" min="1" max="10" /><br />
+    Conhecimento de magia: <input name="magia" type="checkbox" /><br/>
+    Ferramentas de artesão: <input name="ferramentas" type="checkbox" /><br/>
+    <button formaction="?/guerreiros">Inscrever na ordem dos guerreiros</button><br>
+    <button formaction="?/magos">Inscrever no círculo dos magos</button><br>
+    <button formaction="?/artesaos">Inscrever na guilda dos artesãos</button><br>
+</form>
+
+{#if form?.error}
+    <p style="color: red">{form.error}</p>
+{/if}
